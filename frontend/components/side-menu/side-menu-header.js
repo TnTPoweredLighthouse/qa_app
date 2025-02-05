c31b0d3ab3672a37a265f8323ebc57b92ede43c3
OTod <oleks.todorov@gmail.com>
2025-02-05 18:57:03 +0200

    added icons

--- frontend/components/side-menu/side-menu-header.js
+++ frontend/components/side-menu/side-menu-header.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 
 import css from "./side-menu-header.module.css"
 
@@ -9,7 +10,14 @@ export default async function SideMenuHeader() {
         <div className={css.container}>
             <div className={css['categories-container']}>
                 <span>Categories:</span>
-                <Link href={addCategoryHref}>ADD</Link>
+                <Link href={addCategoryHref} className={css['add-link']}>
+                    <Image
+                        width={24}
+                        height={24}
+                        src='/add-circle-svgrepo-com.svg'
+                        alt="add"
+                    />
+                </Link>
             </div>
         </div>
     )
