1f24c5e641c28a53701e84287d707366ea6845b1
OTod <oleks.todorov@gmail.com>
2025-02-05 19:10:47 +0200

    changed format of questions and answers

--- frontend/components/questions/answer.js
+++ frontend/components/questions/answer.js
@@ -3,9 +3,14 @@ import css from './answer.module.css'
 export default function Answer({ body }) {
     return (
         <div className={css.container}>
-            <span className={css.answer}>
-                Answer: {body}
-            </span>
-        </div>
+            <div className={css.answer}>
+
+                <div>Answer: </div>
+
+                <div >
+                    {body}
+                </div>
+            </div>
+        </div >
     )
 }
