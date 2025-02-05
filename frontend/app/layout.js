c6cec8fd8679be8a79f34fda485db88b5324fdc9
OTod <oleks.todorov@gmail.com>
2025-02-05 19:06:00 +0200

    added fonts

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,19 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import css from "@/app/layout.module.css"
 import ReactQueryProvider from "@/components/ReactQueryProvider";
 
 import SideMenu from "@/components/side-menu/side-menu";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '400'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -23,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} `}>
+      <body className={`${roboto.className}`}>
         <ReactQueryProvider>
           <main className={css.container}>
             <div className={css.side}>
