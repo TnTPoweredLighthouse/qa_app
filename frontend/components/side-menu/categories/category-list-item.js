2b2a4e913ea554f0662dc3b74514706c00f426d3
serhii1205 <freemstage@gmail.com>
2025-02-09 20:08:17 +0200

    feat: updating card component

--- frontend/components/side-menu/categories/category-list-item.js
+++ frontend/components/side-menu/categories/category-list-item.js
@@ -1,8 +1,9 @@
-'use client'
-import Link from "next/link"
-import Image from "next/image"
 
-import css from "./category-list-item.module.css"
+import Link from "next/link";
+import Image from "next/image";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function CategoryListItem({ category }) {
     const { name, id } = category
@@ -10,15 +11,18 @@ export default function CategoryListItem({ category }) {
     const categoryDetailsHref = `/categories/${id}`
 
     return (
-        <div className={css.container}>
-            <Link href={categoryDetailsHref}>{name}
-            </Link>
-            <div className={css['management']}>
-                <span className={css['quantity-container']}>{category.questionsQuantity || 'no-data'}</span>
-                <Link className={css['edit-link']} href={categoryEditHref}>
-                    <Image src='/edit-svgrepo-com.svg' alt="add" width={18} height={18}></Image>
-                </Link>
-            </div>
-        </div>
+        <Card sx={{borderRadius: 4, padding: 3, display: 'flex', justifyContent: 'space-between', alignItems: 'center',  }}>
+                <Box sx={{width: 'inherit', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap'}}>
+                    <Link href={categoryDetailsHref}>{name}</Link>
+                </Box>
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                    <Typography component="span" sx={{paddingX: 1}}>{category.questionsQuantity || 'no-data'}</Typography>
+                    <Box sx={{ paddingX: 1 }}>
+                        <Link href={categoryEditHref}>
+                            <Image src='/edit-svgrepo-com.svg' alt="add" width={18} height={18} />
+                        </Link>
+                    </Box>
+                </Box>
+        </Card>
     )
 }
